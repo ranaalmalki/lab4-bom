66fea0233a511ee3efac7172a616d26ae245fb2d
Rana Almalki <ranaalmalki12@gmail.com>
2024-09-15 15:56:50 +0300

    ...not finish i take a lot of time

---
+++ main.js
@@ -0,0 +1,11 @@
+   
+    function openWin() { 
+        myWindow = 
+            window.open("", "", 
+                "width=100, height=100"); 
+    } 
+            
+    function resizeWin() { 
+        myWindow.resizeTo(357, 812); 
+        myWindow.focus(); 
+    } 
